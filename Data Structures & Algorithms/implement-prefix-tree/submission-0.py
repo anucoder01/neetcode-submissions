27349c681d5ed828e49ff861513205efae9a90da
Anuvuti Podder <anuvutipodder205@gmail.com>
2026-08-01 20:54:06 +0530

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,50 @@
+class TrieNode:
+
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+
+        node = self.root
+
+        for ch in word:
+
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+
+            node = node.children[ch]
+
+        node.end = True
+
+    def search(self, word):
+
+        node = self.root
+
+        for ch in word:
+
+            if ch not in node.children:
+                return False
+
+            node = node.children[ch]
+
+        return node.end
+
+    def startsWith(self, prefix):
+
+        node = self.root
+
+        for ch in prefix:
+
+            if ch not in node.children:
+                return False
+
+            node = node.children[ch]
+
+        return True
