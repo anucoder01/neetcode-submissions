087a4daa8ad4eff6d1587fb09559e4b726dd3a6e
Anuvuti Podder <anuvutipodder205@gmail.com>
2026-08-05 19:11:28 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        news = ""
+
+        for char in s:
+            if char.isalnum():
+                news += char.lower()
+
+        return news == news[::-1]
